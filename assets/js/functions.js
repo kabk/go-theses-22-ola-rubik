287cc11bbad5357f6c9078401526af84b4ea49e7
bunny145 <ola.rubik@gmail.com>
2022-03-31 23:22:43 +0200

    ....

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -21,4 +21,21 @@ window.addEventListener( 'DOMContentLoaded', event => {
 	
     const footnote_toggle_ch2 = document.querySelector( '.flip-box-2' );
     footnote_toggle_ch2.addEventListener( 'click', toggleFootnotes );
+    
+    
+    const footnote_toggle_ch3 = document.querySelector( '.flip-box-3' );
+	footnote_toggle_ch3.addEventListener( 'click', toggleFootnotes );
+    
+    
+    const footnote_toggle_ch4 = document.querySelector( '.flip-box-4' );
+	footnote_toggle_ch4.addEventListener( 'click', toggleFootnotes );
+    
+    
+    const footnote_toggle_ch5 = document.querySelector( '.flip-box-5' );
+	footnote_toggle_ch5.addEventListener( 'click', toggleFootnotes );
+    
+    
+    const footnote_toggle_ch6 = document.querySelector( '.flip-box-6' );
+	footnote_toggle_ch6.addEventListener( 'click', toggleFootnotes );
+    
 } );
